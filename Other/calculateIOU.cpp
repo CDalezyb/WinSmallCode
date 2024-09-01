f7598199f5f91962ec5c3c1a19357b24ce225c33
CDale <daleandfire@163.com>
2024-09-01 13:47:06 +0800

    hh

--- Other/calculateIOU.cpp
+++ Other/calculateIOU.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 using namespace std;
+struct Point
+{
+    double x, y;
+    Point(double a, double b) : x(a), y(b) {}
+};
 
 struct Box
 {
-    double x1, x2, y1, y2;
-
-    // assert x1<=x2, y1<=y2
-    Box(double a, double b, double c, double d) : x1(a), x2(b), y1(c), y2(d)
-    {
-        cout << "en" << endl;
-    }
+    Point TL, BR;
+    Box(double a, double b, double c, double d) : TL(a, b), BR(c, d) {}
 };
 
 double calculateIOU(const Box &box1, const Box &box2)
 {
     double insecArea = 0;
 
-    double X1 = max(box1.x1, box2.x1);
-    double X2 = min(box1.x2, box2.x2);
-    double Y1 = max(box1.y1, box2.y1);
-    double Y2 = min(box1.y2, box2.y2);
+    double X1 = max(box1.TL.x, box2.TL.x);
+    double X2 = min(box1.BR.x, box2.BR.x);
+    double Y1 = max(box1.TL.y, box2.TL.y);
+    double Y2 = min(box1.BR.y, box2.BR.y);
 
     insecArea = max(0.0, X2 - X1) * max(0.0, Y2 - Y1);
 
-    double Area1 = (box1.x2 - box1.x1) * (box1.y2 - box1.y1);
-    double Area2 = (box2.x2 - box2.x1) * (box2.y2 - box2.y1);
+    double Area1 = (box1.BR.x - box1.TL.x) * (box1.BR.y - box1.TL.y);
+    double Area2 = (box2.BR.x - box2.TL.x) * (box2.BR.y - box2.TL.y);
 
     return insecArea / (Area1 + Area2 - insecArea);
 }
 
 int main()
 {
-    Box box1(1, 2, 3, 4);
-    Box box2(1, 2, 3, 5);
+    Box box1(1.0, 2.0, 3.0, 4.0);
+    Box box2(1.0, 2.0, 3.0, 5.0);
 
     cout << calculateIOU(box1, box2) << endl;
     return 0;
--- baidu23/timu2 copy.cpp
+++ baidu23/timu2 copy.cpp
@@ -1,104 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
 
-struct SegmentTree
+bool RedTemp(const vector<int> &anger, const vector<int> &diff)
 {
-    vector<int> tree;
-    vector<int> lazy;
-    int n;
-
-    SegmentTree(int size) : n(size), tree(4 * size, 0), lazy(4 * size, 0) {}
-
-    void updateRange(int start, int end, int value, int node = 1, int l = 0, int r = n - 1)
+    int cur_anger = 0;
+    for (int i = 1; i < anger.size(); i++)
     {
-        if (lazy[node] != 0)
-        {
-            tree[node] += lazy[node] * (r - l + 1);
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (start > r || end < l)
-            return;
-        if (start <= l && r <= end)
-        {
-            tree[node] += value * (r - l + 1);
-            if (l != r)
-            {
-                lazy[node * 2] += value;
-                lazy[node * 2 + 1] += value;
-            }
-            return;
-        }
-        int mid = (l + r) / 2;
-        updateRange(start, end, value, node * 2, l, mid);
-        updateRange(start, end, value, node * 2 + 1, mid + 1, r);
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
-    }
-
-    bool checkRange(int start, int end, int threshold, int node = 1, int l = 0, int r = n - 1)
-    {
-        if (start > r || end < l)
-            return false;
-        if (lazy[node] != 0)
-        {
-            tree[node] += lazy[node] * (r - l + 1);
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (start <= l && r <= end)
-        {
-            return tree[node] > threshold * (r - l + 1);
-        }
-        int mid = (l + r) / 2;
-        bool left = checkRange(start, end, threshold, node * 2, l, mid);
-        if (left)
+        cur_anger += diff[i];
+        if (cur_anger > anger[i])
             return true;
-        return checkRange(start, end, threshold, node * 2 + 1, mid + 1, r);
     }
-};
-
+    return false;
+}
 int main()
 {
     int n, m;
     cin >> n >> m;
-    SegmentTree st(n);
-
-    vector<int> anger(n);
-    for (int i = 0; i < n; ++i)
-    {
+    vector<int> anger(n + 1, 0);
+    for (int i = 1; i <= n; i++)
         cin >> anger[i];
-    }
 
-    int lastVersion = -1;
-    for (int i = 1; i <= m; ++i)
+    int l, r;
+    vector<pair<int, int>> operations(m + 1);
+    for (int i = 1; i <= m; i++)
+        cin >> operations[i].first >> operations[i].second;
+
+    int left = 1, right = m, ans = m;
+    vector<int> diff(n + 2, 0);
+    while (left <= right)
     {
-        int l, r;
-        cin >> l >> r;
-        st.updateRange(l - 1, r - 1, 1);
+        int mid = left + (right - left) / 2;
 
-        if (st.checkRange(0, n - 1, *max_element(anger.begin(), anger.end())))
+        fill(diff.begin(), diff.end(), 0);
+        for (int i = 1; i <= mid; i++)
         {
-            lastVersion = i - 1;
-            break;
+            l = operations[i].first, r = operations[i].second;
+            diff[l]++;
+            if (r + 1 <= n + 1)
+                diff[r + 1]--;
+        }
+
+        if (RedTemp(anger, diff))
+        {
+            right = mid - 1;
+            ans = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
         }
     }
 
-    if (lastVersion == -1)
-    {
-        lastVersion = m;
-    }
+    cout << ans << endl;
 
-    cout << lastVersion << endl;
     return 0;
 }
