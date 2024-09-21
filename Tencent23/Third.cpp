37245b98f97dc08973a5faad3abc442c747bbf6f
CDale <daleandfire@163.com>
2024-09-21 18:03:43 +0800

    hh

--- Tencent23/Third.cpp
+++ Tencent23/Third.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <numeric>
-#include <cmath>
 using namespace std;
-/*
-牛牛有一棵二叉树，该二叉树节点的权值为 0 或者 1。
-
-牛牛给你这棵二叉树, 想让你告诉他该二叉树从根节点到叶子节点的所有路径中, 节点"权值 1 的个数"比权值 0 的个数"多 1 的路径有多少条。
-*/
 
 struct TreeNode
 {
@@ -18,70 +10,78 @@ struct TreeNode
     TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
 };
 
-void dfs(TreeNode *root, int count0, int count1, int &ans)
+void buildTree(const vector<int> &nums, const int n, TreeNode *&root)
 {
-    if (root == nullptr)
-        return;
+    queue<TreeNode *> que;
+    root = new TreeNode(nums[0]);
+    que.push(root);
+    int size = 1;
+    int index = 1;
+    while (!que.empty() && index < n)
+    {
+        auto node = que.front();
+        que.pop();
 
-    root->val == 1 ? count1++ : count0++;
+        if (index < n && nums[index] != -1)
+        {
+            node->left = new TreeNode(nums[index]);
+            que.push(node->left);
+        }
+        index++;
+
+        if (index < n && nums[index] != -1)
+        {
+            node->right = new TreeNode(nums[index]);
+            que.push(node->right);
+        }
+        index++;
+    }
+}
+
+int Pcount = 0;
+// int zeroCount = 0, oneCount = 0;
+void Solve(TreeNode *root, int zeroCount, int oneCount)
+{
+    // if (root == nullptr)
+    //     return;
+    if (root->val == 0)
+        zeroCount++;
+    else
+        oneCount++;
 
     if (root->left == nullptr && root->right == nullptr)
     {
-        if (count1 - count0 == 1)
-        {
-            ans++;
-        }
+        if (oneCount - zeroCount == 1)
+            Pcount++;
         return;
     }
 
-    dfs(root->left, count0, count1, ans);
-    dfs(root->right, count0, count1, ans);
+    if (root->left)
+        Solve(root->left, zeroCount, oneCount);
+    if (root->right)
+        Solve(root->right, zeroCount, oneCount);
+
+    return;
 }
 
 int main()
 {
-    int N;
-    cin >> N;
-    vector<int> nums(N, 0);
-    for (int i = 0; i < N; i++)
+    int n;
+    cin >> n;
+    vector<int> nums(n, 0);
+    for (int i = 0; i < n; i++)
         cin >> nums[i];
 
-    // N = 7;
-    // vector<int> nums{1, 0, 0, 1, 0, -1, 1, -1, -1, 1, 0, -1, -1};
-    // vector<int> nums{1, 0, 0, 1, 0, -1, 1};
-
     if (nums.empty())
-        return 0;
-
-    queue<TreeNode *> que;
-    TreeNode *root = new TreeNode(nums[0]);
-    que.push(root);
-    int idx = 1;
-    while (!que.empty() && idx < N)
     {
-        TreeNode *node = que.front();
-        que.pop();
-
-        if (idx < N && nums[idx] != -1)
-        {
-            node->left = new TreeNode(nums[idx]);
-            que.push(node->left);
-        }
-        idx++;
-
-        if (idx < N && nums[idx] != -1)
-        {
-            node->right = new TreeNode(nums[idx]);
-            que.push(node->right);
-        }
-        idx++;
+        cout << 0 << endl;
+        return 0;
     }
 
-    int ans = 0;
+    TreeNode *root;
+    buildTree(nums, n, root);
 
-    dfs(root, 0, 0, ans);
-
-    cout << ans << endl;
-
+    Solve(root, 0, 0);
+    cout << Pcount << endl;
     return 0;
 }
